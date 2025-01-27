ab648b280032b7696239cff7774f037d64ad8c32
Stanislav Brovin <stb@sekai.io>
2025-01-27 20:03:38 +0200

    * update auth data select - local || deploy

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -13,15 +13,29 @@ import { getFirestore } from 'firebase/firestore';
  *  https://firebase.google.com/docs/auth/web/redirect-best-practices?hl=ru&authuser=0&_gl=1*jmoun9*_ga*MTAyNzY2NjU4Ny4xNzM3NzUxMDQy*_ga_CW55HF8NVT*MTczNzc1NDM2My4yLjEuMTczNzc1Njc0OC42MC4wLjA.
  *
  */
-
+const isLocal = false;
 const firebaseConfig = {
-  apiKey: process.env.VITE_REACT_APP_API_KEY,
-  authDomain: process.env.VITE_REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.VITE_REACT_APP_PROJECT_ID,
-  storageBucket: process.env.VITE_REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.VITE_REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.VITE_REACT_APP_APP_ID,
-  measurementId: process.env.VITE_REACT_APP_MEASUREMENT_ID,
+  apiKey: isLocal
+    ? process.env.VITE_REACT_APP_API_KEY
+    : import.meta.env.VITE_REACT_APP_API_KEY,
+  authDomain: isLocal
+    ? process.env.VITE_REACT_APP_AUTH_DOMAIN
+    : import.meta.env.VITE_REACT_APP_AUTH_DOMAIN,
+  projectId: isLocal
+    ? process.env.VITE_REACT_APP_PROJECT_ID
+    : import.meta.env.VITE_REACT_APP_PROJECT_ID,
+  storageBucket: isLocal
+    ? process.env.VITE_REACT_APP_STORAGE_BUCKET
+    : import.meta.env.VITE_REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: isLocal
+    ? process.env.VITE_REACT_APP_MESSAGING_SENDER_ID
+    : import.meta.env.VITE_REACT_APP_MESSAGING_SENDER_ID,
+  appId: isLocal
+    ? process.env.VITE_REACT_APP_APP_ID
+    : import.meta.env.VITE_REACT_APP_APP_ID,
+  measurementId: isLocal
+    ? process.env.VITE_REACT_APP_MEASUREMENT_ID
+    : import.meta.env.VITE_REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
